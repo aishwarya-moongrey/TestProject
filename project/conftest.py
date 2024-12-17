c876498e3ace29b89c3846b3ac642af2c52f3776
aishwarya-moongrey <aishwarya.moongrey@yahoo.com>
2024-12-17 15:28:27 +0530

    first commit

---
+++ project/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager  # Optional, handles driver installation
+
+# Test Setup
+@pytest.fixture(autouse=True)
+def driver():
+    # Setup Chrome driver
+    options = Options()
+    options.headless = False  # Optional: run the browser in the background without UI
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver.get("https://rahulshettyacademy.com/")
+    driver.maximize_window()
+    yield driver
+    driver.quit()
