5cd3d0d3866a83ad3a14a965dec5b56a685b5344
aishwarya-moongrey <aishwarya.moongrey@yahoo.com>
2024-12-24 18:26:49 +0530

    update yaml ubuntu

--- project/conftest.py
+++ project/conftest.py
@@ -10,6 +10,14 @@ def driver():
     # Setup Chrome driver
     options = Options()
     options.add_argument('--headless')
+
+    options.addArguments("start-maximized");# open Browser in maximized mode
+    options.addArguments("disable-infobars"); #// disabling infobars
+    options.addArguments("--disable-extensions"); #// disabling extensions
+    options.addArguments("--disable-gpu"); #// applicable to windows os only
+    options.addArguments("--disable-dev-shm-usage"); #// overcome limited resource problems
+    options.addArguments("--no-sandbox"); #// Bypass OS security model
+
     # options.headless = True  # Optional: run the browser in the background without UI
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.get("https://rahulshettyacademy.com/")
